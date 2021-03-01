3b744ed4107cd5ca0865c31f4d906f5d1a5520ca
sofiia-pavl <72608224+sofiia-pavl@users.noreply.github.com>
2021-03-01 18:28:12 +0200

    Add files via upload

---
+++ UnitTest3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\lab1,3/Fraction.h"
+#include "C:\Users\Софія\source\repos\lab1,3\lab1,3\Fraction.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Fraction d;
+			bool j = d.Init(3, 2);
+			Assert::AreEqual(j, true);
+		}
+	};
+}
