9577fe7bc45effff41041e9942ae2703aebd05b8
sofiia-pavl <72608224+sofiia-pavl@users.noreply.github.com>
2021-03-01 12:39:28 +0200

    Add files via upload

---
+++ Fraction.cpp
@@ -0,0 +1,48 @@
+#include "Fraction.h"
+#include <iostream>
+
+using namespace std;
+
+void Fraction::Display()
+{
+	cout << endl;
+	cout << one << ". " << two << endl;
+	cout << endl;
+	cout << three << ". " << four << endl;
+}
+
+void Fraction::Init(int x, unsigned int y,int z,unsigned int w)
+{
+	one = x;
+	two = y;
+	three = z;
+	four = w;
+}
+
+void Fraction::Read()
+{
+	int x, y,z,w;
+
+	cout << "whole part of first num = "; cin >> x;
+	cout << "fractional part of first num = "; cin >> y;
+	cout << "whole part of second num = "; cin >> z;
+	cout << "fractional part of second num = "; cin >> w;
+
+	Init(x, y, z, w);
+}
+
+void Fraction::Comparison()
+{
+	if (one == three && two == four) cout << one << "."<<two<< " = " << three<<"."<<four;
+	else if (one < three || one == three && two<four)   cout << one << "." << two << " < " << three << "." << four;
+	else if (one > three || one == three && two > four)   cout << one << "." << two << " > " << three << "." << four;
+}
+
+void Fraction::getSub() 
+{
+	int a = one - three;
+	int b = two - four;
+	
+	cout << "subtraction of numbers: ";
+	cout << endl << a << endl << b << endl;
+}
---
+++ Source.cpp
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////
+// Source.cpp
+// головний файл проекту - main
+
+#include <iostream>
+#include "Fraction.h"
+
+
+using namespace std;
+
+int main()
+{
+	
+	Fraction v;
+	v.Read();
+	v.Display();
+	v.getSub() ;
+	v.Comparison();
+
+	return 0;
+}
